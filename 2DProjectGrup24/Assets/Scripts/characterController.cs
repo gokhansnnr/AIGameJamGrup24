b5751ee10b1159b92c1dea552794988267e9a055
Gökhan Şener <49876182+gokhansnnr@users.noreply.github.com>
2024-05-19 16:15:11 +0300

    Hareket Mekanizması + Boş Sahne

--- 2DProjectGrup24/Assets/Scripts/characterController.cs
+++ 2DProjectGrup24/Assets/Scripts/characterController.cs
@@ -10,10 +10,13 @@ public class characterController : MonoBehaviour
     private Animator animator; //Animator bileþeni
     private SpriteRenderer spriteRenderer; //SpriteRenderer bileþeni
 
+    /*
     [Header("Animasyon")]
     [SerializeField] AnimationClip Jump;
     [SerializeField] AnimationClip Fall;
+    */
 
+    bool GroundCheck = false;
     bool canIJump; //Zýplama kontrolü
     Rigidbody2D rb; //Rigidbody
     private float horizontalMovement; //Yatayda hareket
@@ -33,12 +36,12 @@ public class characterController : MonoBehaviour
     private void FixedUpdate()
     {//6
         rb.velocity = new Vector3(horizontalMovement * moveSpeed, rb.velocity.y, 0f); //velocity iþlemleri
-        animator.SetBool("Fall", !GroundCheck()); //Zemine temas yoksa animasyonu uygula
+        animator.SetBool("Fall", !GroundCheck); //Zemine temas yoksa animasyonu uygula
         if(rb.velocity.y < 0.02f)
         { //velocity kontrolü
             rb.AddForce(Vector2.down * 5f, ForceMode2D.Force);
             animator.SetBool("Jump", false); //Atlama animasyonu devre dýþý
-            if (GroundCheck()) //Zemin kontrolü
+            if (GroundCheck) //Zemin kontrolü
             {
                 animator.SetBool("Fall", false);
             }
@@ -49,9 +52,9 @@ public class characterController : MonoBehaviour
     {//4
         horizontalMovement = Input.GetAxisRaw("Horizontal"); //Klavyeden yatay hareketleri aldýk
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.W))
         {//W'ya basýldýðýnda
-            if (GroundCheck()){ //GroundCheck kontrolü yapacak.
+            if (GroundCheck){ //GroundCheck kontrolü yapacak.
                 rb.velocity = new Vector3(rb.velocity.x, JumpForce, 0); //Zýplama yönü tanýmladýk.
                 animator.SetBool("Jump", true); //Zýplama animasyonu verdik.
             }
@@ -77,8 +80,29 @@ public class characterController : MonoBehaviour
         }
     }
 
+    /*
     private bool GroundCheck()
     {//5 - Zemin kontrolü
         return Physics2D.OverlapCircle(transform.position, .5f, LayerMask.GetMask("Ground"));
+    }
+    */
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //GroundCheck = collision.gameObject.CompareTag("Ground") ? true : false;
+        print(GroundCheck);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            GroundCheck = true;
+        }
+        else
+        {
+            GroundCheck = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, .5f);
     }
 }
